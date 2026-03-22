a7f7d76b6fed4ea42c33b63907310166c278b5cc
Uwe <uwe.krien@ifam.fraunhofer.de>
2026-03-22 16:24:44 +0100

    Add investment example

--- tests/test_constraints/test_heat_pump.py
+++ tests/test_constraints/test_heat_pump.py
@@ -7,7 +7,7 @@ from oemof.eesyplan import Project
 from oemof.eesyplan import optimise
 
 
-def test_heat_pump():
+def test_heat_pump_dispatch():
     number = 10
     es = EnergySystem(2023, number=number)
     el_bus = Bus(label="electricity", balanced=False)
@@ -34,6 +34,75 @@ def test_heat_pump():
     flows = results["flow"]
 
     energy = heat_pump.installed_capacity * number
+
+    assert (
+        round(
+            (
+                pd.Series(heat_pump.cop)
+                * flows["electricity"].reset_index(drop=True).squeeze()
+            ).sum(),
+            5,
+        )
+        == energy
+    )
+
+    assert flows["air_source_heat_pump"].sum().iloc[0] == energy
+    assert (
+        round((flows["electricity"].sum() + flows["ambient"].sum()).iloc[0], 5)
+        == energy
+    )
+
+
+def test_heat_pump_investment():
+    number = 10
+    es = EnergySystem(2023, number=number)
+    el_bus = Bus(label="electricity", balanced=False)
+    ambient_bus = Bus(label="ambient", balanced=False)
+    heat_bus = Bus(label="heat", balanced=False)
+    es.add(el_bus, ambient_bus, heat_bus)
+    heat_pump = HeatPump(
+        name="air_source_heat_pump",
+        bus_in_electricity=el_bus,
+        bus_in_heat=ambient_bus,
+        bus_out_heat=heat_bus,
+        maximum_capacity=15,
+        optimize_cap=True,
+        capex_var=100,
+        capex_fix=10,
+        opex_var=-10,
+        opex_fix=1,
+        cop=[3.5, 3.2] * 5,
+        project_data=Project(
+            name="Project_X",
+            lifetime=20,
+            tax=0,
+            discount_factor=0.01,
+        ),
+    )
+
+    es.add(heat_pump)
+    results = optimise(es)
+
+    # Check annuity
+    annuity = 83.123
+    assert (
+        round(
+            (
+                results["objective"]
+                + results["invest"].squeeze() * heat_pump.capex_var
+                - results["invest"].squeeze() * heat_pump.opex_fix
+            ),
+            3,
+        )
+        == annuity
+    )
+
+    # check objective
+    assert round(results["objective"], 3) == -1401.877
+
+    flows = results["flow"]
+
+    energy = results["invest"].squeeze() * number
 
     assert (
         round(
